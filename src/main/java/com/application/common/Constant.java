90cb455da17bfda38ca12943bab0aa1adebf0c35
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2025-02-01 20:12:18 +0900

    [REFECTOR] 더미 코드 제거

--- src/main/java/com/application/common/Constant.java
+++ src/main/java/com/application/common/Constant.java
@@ -2,7 +2,6 @@ package com.application.common;
 
 public class Constant {
 
-    public final static String FRONT_CALLBACK_URI = "myapp://callback?uuid=";  //
     public final static String POST_CLIENT_TOKEN_CALLBACK_URI = "https://localhost:8080/api/token"; //ex) https://localhost:3000/api/token 로그인 성공 후 토큰값 전달
     public final static String FRONT_SEND_REDIRECT = "http://localhost:8080/api/test"; // ex) "http://localhost:3000/api/test" 로그인 성공 후 리다이렉트
     public final static String FRONT_LOGIN_FAIL_REDIRECT = "http://localhost:8080/login"; // ex) 로그인 실패시 리다이렉트
